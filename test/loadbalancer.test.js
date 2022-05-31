4168eaaabdb96e6bb21c584489cd06a81908a548
yilwang <liyaysw@gmail.com>
2022-05-31 11:16:22 -0600

    correct the loadbalancer test

--- test/loadbalancer.test.js
+++ test/loadbalancer.test.js
@@ -2,7 +2,8 @@ import fetch from "node-fetch";
 import assert from 'assert';
 
 it('Should get login token', async() => {
-    const token = await fetch ('http://35.233.139.110');
-    console.log("Token" + token);
-    assert.equal(answer,"Hello");
+    const reponse = await fetch ('http://35.233.139.110');
+    const text= await reponse.text();
+    console.log("Token: " + text);
+    assert.equal(text,"Hello");
 })
